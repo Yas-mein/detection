9f5a164c97ac997aadb2c4a8a71ca264f93051ac
Yas-mein <yasooahmad138@gmail.com>
2023-05-18 03:31:06 +0300

    added all features -1

--- detectobject/urls.py
+++ detectobject/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
-# from . import views
-# from . import api 
-# from .views import object_detection
-# from .views import ObjectDetectionView
+
 from .views import api2
 
 urlpatterns = [
-    # path('d/', ObjectDetectionView),
-    path('a/',api2),
+    path('',api2),
 
 ]
 
